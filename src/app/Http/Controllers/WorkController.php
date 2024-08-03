3ce91c85dfc90d53a3905b9844ede5f7616cab03
kozuka yuko <yuzixiaozhong@gmail.com>
2024-08-03 20:04:02 +0900

    日付をまたいだ時に新しいレコードの作成

--- src/app/Http/Controllers/WorkController.php
+++ src/app/Http/Controllers/WorkController.php
@@ -12,12 +12,37 @@ use Carbon\Carbon;
 
 class WorkController extends Controller
 {
+
+    public function newRecordAtMidnight(){
+        $user = Auth::user();
+        $now = Carbon::now();
+        $yesterday = $now->copy()->subDay()->format('Y-m-d');
+        $breaking = Breaking::whereDate('created_at', $yesterday)->get();
+        $work = Work::whereDate('created_at', $yesterday)->whereNull('work_end')->first();
+
+        if($now->format('H:i:s') === '00:00:00'){
+            $allBreakingTime = $breaking->sum('breaking_time');
+            $workTime = $work->work_end - $work->work_start - $allBreakingTime;
+                
+            $work->update([
+                'work_end' => '23:59:59',
+                'allbreaking_time' => $allBreakingTime,
+                'work_time' => $workTime
+            ]);
+
+            Work::create([
+                'user_id' => $user->id,
+                'work_date' => $now->format('Y_m_d'),
+                'work_start' => $now->secondsSinceMidnight()
+            ]);
+        } 
+    }
+
     public function index()
     {
         $user = Auth::user();
-        $today = Carbon::today();
-        $newWork = Work::where('user_id', $user->id)->whereDate('created_at', $today)->orderBy('created_at', 'desc')->first();
-        $newBreaking = Breaking::where('work_id', $newWork->id)->whereDate('created_at', $today)->orderBy('created_at', 'desc')->first();
+        $newWork = Work::where('user_id', $user->id)->orderBy('created_at', 'desc')->first();
+        $newBreaking = Breaking::where('work_id', $newWork->id)->orderBy('created_at', 'desc')->first();
 
         $isWorkStartDisabled = $newWork && $newWork->work_start !== null && $newWork->work_end === null;
         $isWorkEndDisabled = !$isWorkStartDisabled || ($newWork && $newWork->work_end !== null);
@@ -46,43 +71,16 @@ class WorkController extends Controller
 
         $work = Work::where('user_id', $userId)->orderBy('created_at', 'desc')->first();
 
-        if ($work->work_start !== Carbon::parse('00:00:00')->secondsSinceMidnight()) {
-            $work->update(['work_end' => $now->secondsSinceMidnight()]);
-        } else {
-            $work->update(['work_end' => Carbon::parse('23:59:59')->secondsSinceMidnight()]);
-            $newWork = Work::create([
-                'user_id' => $userId,
-                'work_date' => $now->format('Y_m_d'),
-                'work_start' => Carbon::parse('00:00:00')->secondsSinceMidnight(),
-                'work_end' => $now->secondsSinceMidnight()
-            ]);
-        }
-
         $breaking = Breaking::where('work_id', $work->id)->get();
-        $newBreaking = Breaking::where('work_id', $work->id + 1)->get();
 
-        if ($work->work_start !== Carbon::parse('00:00:00')->secondsSinceMidnight()) {
-            $allBreakingTime = $breaking->sum('breaking_time');
-            $work_time = $work->work_end - $work->work_start - $allBreakingTime;
-            $work->update([
-                'allbreaking_time' => $allBreakingTime,
-                'work_time' => $work_time
-            ]);
-        } else {
-            $y_allBreakingTime = $breaking->sum('breaking_time');
-            $y_workTime = $work->work_end - $work->work_start - $y_allBreakingTime;
-            $work->update([
-                'allbreaking_time' => $y_allBreakingTime,
-                'work_time' => $y_workTime
-            ]);
-
-            $t_allBreakingTime = $newBreaking->sum('breaking_time');
-            $t_workTime = $newWork->work_end - $t_allBreakingTime;
-            $newWork->update([
-                'allbreaking_time' => $t_allBreakingTime,
-                'work_time' => $t_workTime
-            ]);
-        }
+        $allBreakingTime = $breaking->sum('breaking_time');
+        $work_time = $work->work_end - $work->work_start - $allBreakingTime;
+        $work->update([
+            'work_end' => $now->secondsSinceMidnight(),
+            'allbreaking_time' => $allBreakingTime,
+            'work_time' => $work_time
+        ]);
+        
         return redirect('/');
     }
 
